c4d3127c787f9e1667fc884a6fd3050f702acf88
HansaMM <hansanor@protonmail.com>
2024-03-13 12:54:13 +0100

    fixed

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function setStage(newStage) {
- const form = document.querySelector("multi-step")
+ const form = document.querySelector(".multi-step")
 
  form.classList.remove("meny-nåverende-1")
  form.classList.remove("meny-nåverende-2")
  form.classList.remove("meny-nåverende-3")
  form.classList.remove("meny-nåverende-4")
 
- const newClass = "meny-nåverende" + newStage
+ const newClass = "meny-nåverende-" + newStage
  form.classList.add(newClass)
 
 
