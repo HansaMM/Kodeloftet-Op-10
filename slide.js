ead0a325630b7f3ae9808f36b6d8685d46fe1a9c
HansaMM <hansanor@protonmail.com>
2024-03-22 12:52:55 +0100

    java fix

--- slide.js
+++ slide.js
@@ -1,16 +1,3 @@
-function setStage(newStage) {
- const form = document.querySelector(".multi-step")
-
- form.classList.remove("meny-nåverende-1")
- form.classList.remove("meny-nåverende-2")
- form.classList.remove("meny-nåverende-3")
- form.classList.remove("meny-nåverende-4")
-
- const newClass = "meny-nåverende-" + newStage
- form.classList.add(newClass)
- 
-}
-
 var currentImage = 1;
 showDivs(currentImage);
 
